038aa8bb5bbbc3d354ca86cbf47bea3070960fe5
joao_lima <joaocsantoslima@gmail.com>
2024-05-24 20:32:59 -0300

    appcinema2

--- bp.py
+++ bp.py
@@ -1,56 +1,50 @@
+# importa biblioteca
+import os
+
 # nome e idade do usuário
 nome = input('Informe o seu nome:')
 idade = int(input('Informe a sua idade:'))
 
-# lista de salas e filmes
-print('Lista de filmes:\n')
-print('Sala 1 - A Princesa e O Sapo;')
-print('Sala 2 - Velozes e Furiosos 1;')
-print('Sala 3 - 2012;')
-print('Sala 4 - Coraline;')
-print('Sala 5 - A Casa Monstro;')
+# limpa console
+os.system('cls')
 
-# usuário informa a sala
-sala = int(input('Informe a sala desejada:'))
+# inicia o loop
+while True:
+    # lista de salas e filmes
+    print(f'{'='*20}CINE COBRA{'='*20}\n')
+    print(f'{'-'*20}Lista de filmes:{'-'*20}\n')
+    print('Sala 1 - A Princesa e O Sapo - Livre;')
+    print('Sala 2 - Velozes e Furiosos 1 - 18 anos;')
+    print('Sala 3 - 2012 - 18 anos;')
+    print('Sala 4 - Coraline - 16 anos;')
+    print('Sala 5 - A Casa Monstro - 16 anos;')
 
-match sala: 
-    case 1:
-        print(f'Filme escolhido por {nome}: A Princesa e O Sapo.')
-    case 2:
-        print(f'Filme esolhido por {nome}: Velozes e Furiosos 1.')
-    case 3:
-        print(f'Filme escolhido por {nome}: 2012.')
-    case 4:
-        print(f'Filme escolhido por {nome}: Coraline.')
-    case 5:
-        print(f'Filme escolhido por {nome}: A Casa Monstro.')
-    case _:
-        print('Sala inexistente')
-    
-Princesa = print('A Princesa e O Sapo.')
-Velozes = print('Velozes e Furiosos 1.')
-Doze = print('2012.')
-Coraline = print('Coraline')
-Casa = print('A Casa Monstro.')
+    # usuário informa a sala
+    sala = input('Informe a sala desejada:')
 
-# classificação indicativa
-if idade >= 18: 
-    print(f'{nome} está liberado para ver {Velozes}.')
-    print(f'{nome} está liberado para ver {Doze}.')
-    print (f'{nome} está liberado para ver {Princesa}.')
-    print(f'{nome} está liberado para ver {Coraline}.')
-    print(f'{nome} está liberado para ver {Casa}.')
-elif idade >= 16:
-    print(f'{nome} não está liberado para ver {Velozes}.')
-    print(f'{nome} não está liberado para ver {Doze}.')
-    print(f'{nome} está liberado para ver {Coraline}.')
-    print(f'{nome} está liberado para ver {Casa}.')
-    print(f'{nome} está liberado para ver {Princesa}.')
-else:
-    print(f'{nome} não está liberado para ver {Velozes}.')
-    print(f'{nome} não está liberado para ver {Doze}.')
-    print(f'{nome} não está liberado para ver {Coraline}.')
-    print(f'{nome} não está liberado para ver {Casa}.')
-    print(f'{nome} está liberado para ver {Princesa}.')
+    # limpa console
+    os.system('cls')
 
-    
+    # verifica a sala e a idade
+    match sala:
+        case '1':
+            idade_minima = 0
+        case '2':
+            idade_minima = 18
+        case '3':
+            idade_minima = 18
+        case '4':
+            idade_minima = 16
+        case '5':
+            idade_minima = 16
+        case _:
+            print('Sala inexistente.')
+            continue
+
+    if idade >= idade_minima:
+        print(f'Ingresso liberado para {nome}. Bom filme!')
+        break
+    else:
+        print(f'Entrada não permitida para {nome}. Favor escolher outro filme!')
+        continue
+
